e4977d0084fd9b84fb1ad8fd05aa9d0466cda42d
Adam Khalifa <70577640+geekMe1982@users.noreply.github.com>
2024-02-22 20:33:56 +0300

    Initial Commit

---
+++ Networking101/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Networking101
+//
+//  Created by Adam Khalifa on 22.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Networking101/Networking101App.swift
@@ -0,0 +1,17 @@
+//
+//  Networking101App.swift
+//  Networking101
+//
+//  Created by Adam Khalifa on 22.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Networking101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
