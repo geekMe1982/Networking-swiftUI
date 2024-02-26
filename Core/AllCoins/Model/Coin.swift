41cbf6af3d83193af2b1445a1725c7c79e27797e
Adam Khalifa <70577640+geekMe1982@users.noreply.github.com>
2024-02-26 21:58:04 +0300

    add model

---
+++ Core/AllCoins/Model/Coin.swift
@@ -0,0 +1,16 @@
+//
+//  Coin.swift
+//  Networking101
+//
+//  Created by Adam Khalifa on 26.02.2024.
+//
+
+import Foundation
+
+struct Coin: Codable {
+    let id: String
+    let symbol: String
+    let name: String
+    let currentPrice: Double
+    let marketCapRank: Int
+}
