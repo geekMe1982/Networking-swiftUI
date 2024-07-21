a4f8974ca273bc92d02f7e18d9bebcac87de03fb
Adam Khalifa <70577640+geekMe1982@users.noreply.github.com>
2024-07-21 16:52:31 +0300

    .

--- Core/AllCoins/Service/CoinDataService.swift
+++ Core/AllCoins/Service/CoinDataService.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class CoinDataService {
     
-    private let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&price_change_percentage=24h&locale=en"
+    private let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h&locale=en"
     
     func fetchCoins(completion: @escaping(Result<[Coin], Error>) -> Void) {
         guard let url = URL(string: urlString) else {return}
@@ -24,10 +24,10 @@ class CoinDataService {
             guard let coins = try? JSONDecoder().decode([Coin].self, from: data) else {return}
 
             for coin in coins {
-                print(coin.currentPrice)
+                print(coin)
+                
             }
             completion(.success(coins))
-        }
-        .resume()
+        }.resume()
     }
 }
